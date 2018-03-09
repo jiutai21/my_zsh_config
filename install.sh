908ac7826b854cf2e66ba3d3af5a21598119971e
caozeyu <caozy@foxmail.com>
2018-03-09 10:59:03 +0800

    我的zsh 配置

---
+++ install.sh
@@ -0,0 +1,8 @@
+$ZSH_AUTOSUGGESTIONS="https://github.com/zsh-users/zsh-autosuggestions.git"
+$ZSH_SYNTAX_HIGHLIGHTING="https://github.com/zsh-users/zsh-syntax-highlighting.git"
+
+git clone $ZSH_AUTOSUGGESTIONS ~/.oh-my-zsh/custom/plugins zsh-autosuggestions
+git clone $ZSH_SYNTAX_HIGHLIGHTING ~/.oh-my-zsh/custom/plugins zsh-syntax-highlighting
+
+cp zshrc ~/.zshrc
+source ~/.zshrc
