746928a19f7e00e2d2d97d0727227b63028fe4f5
jiutai <caoczy@foxmail.com>
2018-04-22 21:42:59 +0800

    修复BUG

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
-$ZSH_AUTOSUGGESTIONS="https://github.com/zsh-users/zsh-autosuggestions.git"
-$ZSH_SYNTAX_HIGHLIGHTING="https://github.com/zsh-users/zsh-syntax-highlighting.git"
+#!/bin/bash
 
-git clone $ZSH_AUTOSUGGESTIONS ~/.oh-my-zsh/custom/plugins zsh-autosuggestions
-git clone $ZSH_SYNTAX_HIGHLIGHTING ~/.oh-my-zsh/custom/plugins zsh-syntax-highlighting
+ZSH_AUTOSUGGESTIONS="https://github.com/zsh-users/zsh-autosuggestions.git"
+ZSH_SYNTAX_HIGHLIGHTING="https://github.com/zsh-users/zsh-syntax-highlighting.git"
+
+git clone $ZSH_AUTOSUGGESTIONS ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone $ZSH_SYNTAX_HIGHLIGHTING ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 cp zshrc ~/.zshrc
-source ~/.zshrc
